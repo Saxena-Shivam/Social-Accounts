feb3b9ddf299141544ae8a96a42bda852b6c22e6
shivamsaxena56 <shivamsaxena862006@gmail.com>
2025-05-15 18:49:30 +0530

    config file updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "tailwindcss";
+
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: parseInt(import.meta.env.VITE_PORT) || 5173,
+  },
   plugins: [react(), tailwindcss()],
 });
